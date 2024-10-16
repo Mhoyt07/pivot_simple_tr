a73408113733edda86d11bededf729330c859e7f
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2024-10-16 12:37:01 -0500

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+
+    //pivot Constants
+    public static final int pivot_motor_id = 0;
+    public static final double max_pivot_motor_speed = 0.5;
+}
